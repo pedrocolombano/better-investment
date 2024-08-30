385ae2d1ac1bbdd9edcfda6c58649eaa8d0265c0
Pedro Colombano <pedromaiacolombano@gmail.com>
2024-08-30 13:47:43 -0300

    feat: project created

---
+++ src/main/java/com/pedrocolombano/betterinvestment/BetterInvestmentApplication.java
@@ -0,0 +1,13 @@
+package com.pedrocolombano.betterinvestment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterInvestmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetterInvestmentApplication.class, args);
+    }
+
+}
